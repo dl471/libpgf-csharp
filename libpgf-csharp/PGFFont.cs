dcd63a15d6664e74abcac10a5a55a6a5d16323e4
dl471 <annamika@bk.ru>
2019-03-21 00:02:28 +0000

    Add PGFFont

---
+++ libpgf-csharp/PGFFont.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace libpgf_csharp
+{
+    class PGFFont
+    {
+
+        private PGFFontRaw rawFont;
+        private IntPtr fontPtr;
+
+        public PGFFont(IntPtr ptrToFont)
+        {
+            fontPtr = ptrToFont;
+            rawFont = Marshal.PtrToStructure<PGFFontRaw>(ptrToFont);
+        }
+
+        public void SaveFont()
+        {
+            SaveFont(fontPtr);
+        }
+
+        public void SaveFont(IntPtr ptrToFont)
+        {
+            Marshal.StructureToPtr<PGFFontRaw>(rawFont, ptrToFont, false);
+        }
+
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct PGFFontRaw
+    {
+        IntPtr header;
+
+        IntPtr dimension;
+        IntPtr bearingX;
+        IntPtr bearingY;
+        IntPtr advance;
+
+        IntPtr charmap;
+        IntPtr charptr;
+        IntPtr shadowmap;
+
+        IntPtr glyphdata;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 65536)]
+        IntPtr[] char_glpyh;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 512)]
+        IntPtr[] shadow_glpyh;
+    }
+
+    internal struct F26_Pair
+    {
+        int h;
+        int v;
+    }
+}
