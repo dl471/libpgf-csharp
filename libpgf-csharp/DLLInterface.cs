e111131656c3bb6b28cb9ab449c80e8a026b0d52
dl471 <annamika@bk.ru>
2019-03-21 00:34:51 +0000

    Add DLL interface

---
+++ libpgf-csharp/DLLInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace libpgf_csharp
+{
+    internal static class DLLInterface
+    {
+        [DllImport("libpgfglyph.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr LoadFont(string fileName);
+
+        [DllImport("libpgfglyph.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool SaveFont(IntPtr fontPtr, string FileName);
+    }
+}
