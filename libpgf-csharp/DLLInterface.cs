bb5f90daf7f11671a93d9b6c6ce4c58aa49c483f
dl471 <annamika@bk.ru>
2019-03-27 01:44:46 +0000

    Update DLL name
    -Update DLL name in line with name change in source library

--- libpgf-csharp/DLLInterface.cs
+++ libpgf-csharp/DLLInterface.cs
@@ -5,10 +5,10 @@ namespace libpgf_csharp
 {
     internal static class DLLInterface
     {
-        [DllImport("libpgfglyph.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libpgfdll.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr LoadFont(string fileName);
 
-        [DllImport("libpgfglyph.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("libpgfdll.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool SaveFont(IntPtr fontPtr, string FileName);
     }
 }
